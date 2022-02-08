b0c0003534919c7d391438807637c6fe5ebbba14
0-vortex <0-vortex@users.noreply.github.com>
2022-02-08 04:47:34 +0000

    chore: seed supabase data [skip ci]

--- src/seed/user_stars.sql
+++ src/seed/user_stars.sql
@@ -1,5 +1,5 @@
 --
--- Data for Name: user_stars; Type: TABLE DATA; Schema: public; Timestamp: 2022-02-08T02:48:49.812Z
+-- Data for Name: user_stars; Type: TABLE DATA; Schema: public; Timestamp: 2022-02-08T04:47:33.196Z
 -- COPY user_stars(id, user_id, star_id, repo_name, recency_score, description, issues, stars, contributors, created_at) FROM 'user_stars.csv' WITH DELIMITER ',' CSV HEADER;
 --
 
@@ -2084,8 +2084,8 @@ INSERT INTO user_stars(id, user_id, star_id, repo_name, recency_score, descripti
 (2039, 17373317, 8459994, 'google/google.github.io', 11, null, 0, 211, ['willnorris','dazuma'], '2022-01-20T23:57:45.557786'),
 (2040, 43640229, 223971726, 'ryanheise/just_audio', 17, 'Audio Player', 119, 559, ['ryanheise','bdlukaa'], '2022-01-20T23:57:45.994286'),
 (2041, 17373317, 20723540, 'microsoft/microsoft.github.io', 12, 'Microsoft on GitHub', 0, 2081, ['vinhub','martinwoodward'], '2022-01-20T23:57:46.319376'),
-(2043, 19617280, 191414235, 'orbit-love/orbit-model', 16, 'A framework for building high gravity communities 🪐', 4, 778, ['dzello','patrickjwoods'], '2022-01-20T23:57:46.503758'),
 (2042, 505751, 290669864, 'KengoTODA/actions-setup-docker-compose', 14, 'the GitHub Action setting up docker-compose command', 3, 2, ['KengoTODA','semantic-release-bot'], '2022-01-20T23:57:46.405469'),
+(2043, 19617280, 191414235, 'orbit-love/orbit-model', 16, 'A framework for building high gravity communities 🪐', 4, 778, ['dzello','patrickjwoods'], '2022-01-20T23:57:46.503758'),
 (2044, 43640229, 159527907, 'ryanheise/audio_service', 18, 'Flutter plugin to play audio in the background while the screen is off.', 105, 631, ['ryanheise','nt4f04uNd'], '2022-01-20T23:57:46.719903'),
 (2045, 19617280, 304344049, 'sveltejs/kit', 17, 'The fastest way to build Svelte apps', 370, 6597, ['Rich-Harris','benmccann'], '2022-01-20T23:57:47.212358'),
 (2046, 17373317, 3310609, 'Netflix/netflix.github.com', 13, null, 36, 1152, ['Randgalt','PoorImpulseCtrl'], '2022-01-20T23:57:47.831285'),
@@ -10946,8 +10946,8 @@ INSERT INTO user_stars(id, user_id, star_id, repo_name, recency_score, descripti
 (10937, 3846874, 52630616, 'apollographql/apollo-client', 0, ':rocket:  A fully-featured, production ready caching GraphQL client for every UI framework and GraphQL server.', 572, 17199, ['benjamn','renovate-bot'], '2022-01-22T13:06:04.999987'),
 (10938, 38838958, 156914027, 'isaachinman/next-i18next', 1, 'The easiest way to translate your NextJs apps.', 22, 2917, ['isaachinman','capellini'], '2022-01-22T13:06:06.331489'),
 (10939, 40713709, 73184139, 'fkhadra/react-toastify', 1, 'React notification made easy 🚀 !', 54, 8288, ['fkhadra','rhdeck'], '2022-01-22T13:06:06.481045'),
-(10940, 3846874, 14210523, 'yargs/yargs', 1, ' yargs the modern, pirate-themed successor to optimist.', 300, 9461, ['bcoe','substack'], '2022-01-22T13:06:06.762484'),
 (10941, 38838958, 383845459, 'lorenaaze/codewars.desafios', 2, 'Solução de desafios encontrados na plataforma codewars ', 0, 4, ['lorenaaze'], '2022-01-22T13:06:07.947139'),
+(10940, 3846874, 14210523, 'yargs/yargs', 1, ' yargs the modern, pirate-themed successor to optimist.', 300, 9461, ['bcoe','substack'], '2022-01-22T13:06:06.762484'),
 (10942, 40713709, 367602396, 'prince776/discode', 2, 'A web app to code collaboratively with voice chat in real time. Discord + coding = Discode', 2, 69, ['prince776','HarshKapadia2'], '2022-01-22T13:06:08.10182'),
 (10943, 3846874, 1341324, 'isaacs/rimraf', 2, 'A `rm -rf` util for nodejs', 63, 4570, ['isaacs','PeterDaveHello'], '2022-01-22T13:06:08.28582'),
 (10944, 38838958, 34134519, 'awvalenti/materialdidatico', 3, 'Material didático para assuntos gerais de desenvolvimento de software', 0, 4, ['awvalenti'], '2022-01-22T13:06:09.463249'),
