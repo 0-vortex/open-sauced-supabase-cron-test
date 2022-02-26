cf80180db0ca26dd7917724df87e157a6b96d0d9
0-vortex <0-vortex@users.noreply.github.com>
2022-02-26 08:46:18 +0000

    chore: seed supabase data [skip ci]

--- src/seed/recommendations.sql
+++ src/seed/recommendations.sql
@@ -1,5 +1,5 @@
 --
--- Data for Name: recommendations; Type: TABLE DATA; Schema: public; Timestamp: 2022-02-26T06:49:35.734Z
+-- Data for Name: recommendations; Type: TABLE DATA; Schema: public; Timestamp: 2022-02-26T08:46:17.244Z
 -- COPY recommendations(id, stars, issues, total_stars, votes, avg_recency_score, repo_name, description, contributors, created_at) FROM 'recommendations.csv' WITH DELIMITER ',' CSV HEADER;
 --
 
