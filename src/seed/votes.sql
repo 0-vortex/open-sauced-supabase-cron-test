8daea320465ec58209986c29092119e5b7f4b19a
0-vortex <0-vortex@users.noreply.github.com>
2022-03-06 22:44:18 +0000

    chore: seed supabase data [skip ci]

--- src/seed/votes.sql
+++ src/seed/votes.sql
@@ -1,5 +1,5 @@
 --
--- Data for Name: votes; Type: TABLE DATA; Schema: public; Timestamp: 2022-03-06T20:44:17.274Z
+-- Data for Name: votes; Type: TABLE DATA; Schema: public; Timestamp: 2022-03-06T22:44:17.343Z
 -- COPY votes(id, github_user_id, repo_name, code, created_at) FROM 'votes.csv' WITH DELIMITER ',' CSV HEADER;
 --
 
