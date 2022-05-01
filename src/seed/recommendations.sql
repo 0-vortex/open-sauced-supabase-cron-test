f2bddcdd224be9febeee17fa895485af98b9495a
0-vortex <0-vortex@users.noreply.github.com>
2022-05-01 06:48:14 +0000

    chore: seed supabase data [skip ci]

--- src/seed/recommendations.sql
+++ src/seed/recommendations.sql
@@ -1,5 +1,5 @@
 --
--- Data for Name: recommendations; Type: TABLE DATA; Schema: public; Timestamp: 2022-05-01T04:54:44.764Z
+-- Data for Name: recommendations; Type: TABLE DATA; Schema: public; Timestamp: 2022-05-01T06:48:12.506Z
 -- COPY recommendations(id, stars, issues, total_stars, votes, avg_recency_score, repo_name, description, contributors, created_at) FROM 'recommendations.csv' WITH DELIMITER ',' CSV HEADER;
 --
 
@@ -2348,9 +2348,9 @@ INSERT INTO recommendations(id, stars, issues, total_stars, votes, avg_recency_s
 (238039731, 105, 103, 1, 0, 8, 'redwoodjs/redwoodjs.com', 'Public website for RedwoodJS', ARRAY['thedavidprice', 'cannikin'], '2022-02-23T23:35:41.043146+00:00'),
 (238197045, 196, 10, 1, 0, 22, 'bradtraversy/react_native_shopping_list', 'React Native app from crash course', ARRAY['bradtraversy', 'leosuncin'], '2022-02-23T23:35:41.043146+00:00'),
 (238514517, 2706, 24, 2, 0, 13, 'matteobruni/tsparticles', 'tsParticles - Easily create highly customizable particles animations and use them as animated backgrounds for your website. Ready to use components available for React.js, Vue.js (2.x and 3.x), Angular, Svelte, jQuery, Preact, Inferno, Solid, Riot and Web Components.', ARRAY['matteobruni', 'VincentGarreau'], '2022-02-23T23:35:41.043146+00:00'),
+(238537897, 31, 7, 1, 0, 9, 'michal-wrzosek/cntl', 'Small library that helps you write your class names in a more clear, readable and composable way. Especially useful with tailwind css.', ARRAY['michal-wrzosek', 'brightpixels'], '2022-02-23T23:35:41.043146+00:00'),
 (238851101, 2263, 0, 1, 0, 12, 'naver/fe-news', 'FE 기술 소식 큐레이션 뉴스레터', ARRAY['netil', 'sculove'], '2022-02-23T23:35:41.043146+00:00'),
 (238892079, 382, 45, 1, 0, 13, 'mirumee/saleor-platform', 'All Saleor services started from a single repository with docker-compose.', ARRAY['maarcingebala', 'tomaszszymanski129'], '2022-02-23T23:35:41.043146+00:00'),
-(238537897, 31, 7, 1, 0, 9, 'michal-wrzosek/cntl', 'Small library that helps you write your class names in a more clear, readable and composable way. Especially useful with tailwind css.', ARRAY['michal-wrzosek', 'brightpixels'], '2022-02-23T23:35:41.043146+00:00'),
 (239150878, 84, 2, 1, 0, 28, 'wobsoriano/coderstats', '📊 Data visualizations of your top languages, starred repositories and top repos.', ARRAY['wobsoriano'], '2022-02-23T23:35:41.043146+00:00'),
 (239002506, 953, 17, 1, 0, 27, 'sbstp/kubie', 'A more powerful alternative to kubectx and kubens', ARRAY['sbstp', 'TyBrown'], '2022-02-23T23:35:41.043146+00:00'),
 (239233346, 1467, 11, 1, 0, 11, 'ardalis/ApiEndpoints', 'A project for supporting API Endpoints in ASP.NET Core web applications.', ARRAY['ardalis', 'maxkoshevoi'], '2022-02-23T23:35:41.043146+00:00'),
