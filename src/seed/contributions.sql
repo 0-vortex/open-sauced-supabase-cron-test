4bdcb586e7143238178a73158e6386416569978e
0-vortex <0-vortex@users.noreply.github.com>
2022-05-20 20:46:15 +0000

    chore: seed supabase data [skip ci]

--- src/seed/contributions.sql
+++ src/seed/contributions.sql
@@ -1,5 +1,5 @@
 --
--- Data for Name: contributions; Type: TABLE DATA; Schema: public; Timestamp: 2022-05-20T18:48:28.844Z
+-- Data for Name: contributions; Type: TABLE DATA; Schema: public; Timestamp: 2022-05-20T20:46:12.991Z
 -- COPY contributions(id, repo_id, count, last_merged_at, contributor, url) FROM 'contributions.csv' WITH DELIMITER ',' CSV HEADER;
 --
 
@@ -1580,7 +1580,6 @@ INSERT INTO contributions(id, repo_id, count, last_merged_at, contributor, url) 
 (1879, 90408889, 4, '2022-01-09T09:51:32', 'icebob', 'https://github.com/moleculerjs/moleculer-web/pull/292'),
 (1880, 90408889, 1, '2020-02-16T19:03:48', 'tobydeh', 'https://github.com/moleculerjs/moleculer-web/pull/164'),
 (1881, 90408889, 1, '2018-11-27T18:53:54', 'sibblegp', 'https://github.com/moleculerjs/moleculer-web/pull/88'),
-(1882, 90408889, 1, '2020-11-25T21:32:48', 'mariusbackes', 'https://github.com/moleculerjs/moleculer-web/pull/214'),
 (1883, 90408889, 3, '2020-06-21T19:55:02', 'intech', 'https://github.com/moleculerjs/moleculer-web/pull/187'),
 (1884, 28548699, 1, '2015-01-09T21:06:18', 'a--hoang', 'https://github.com/cassidoo/getting-a-gig/pull/5'),
 (1885, 90408889, 1, '2019-03-04T19:52:17', 'edgardoalz', 'https://github.com/moleculerjs/moleculer-web/pull/103'),
@@ -1921,6 +1920,7 @@ INSERT INTO contributions(id, repo_id, count, last_merged_at, contributor, url) 
 (2220, 257314632, 4, '2022-03-16T21:58:09', '10thfloor', 'https://github.com/onflow/fcl-js/pull/956'),
 (2221, 51568094, 1, '2021-02-28T02:57:59', 'chengjieyun59', 'https://github.com/careercup/CtCI-6th-Edition-JavaScript/pull/80'),
 (2222, 257314632, 3, '2020-07-08T18:25:47', 'cybercent', 'https://github.com/onflow/fcl-js/pull/147'),
+(1882, 90408889, 1, '2020-11-25T21:32:48', 'mariusbackes', 'https://github.com/moleculerjs/moleculer-web/pull/214'),
 (2223, 257314632, 4, '2022-05-06T18:36:02', 'gregsantos', 'https://github.com/onflow/fcl-js/pull/1165'),
 (2224, 257314632, 2, '2020-09-07T02:59:44', 'psiemens', 'https://github.com/onflow/fcl-js/pull/358'),
 (2225, 51568094, 1, '2021-06-02T15:31:08', 'kapilraghuwanshi', 'https://github.com/careercup/CtCI-6th-Edition-JavaScript/pull/82'),
@@ -2532,8 +2532,6 @@ INSERT INTO contributions(id, repo_id, count, last_merged_at, contributor, url) 
 (20, 278335273, 1, '2022-05-03T08:12:50', 'thetayloredman', 'https://github.com/anuraghazra/github-readme-stats/pull/1350'),
 (35, 278335273, 2, '2020-08-30T12:53:59', 'imrdong', 'https://github.com/anuraghazra/github-readme-stats/pull/418'),
 (29, 278335273, 3, '2020-09-19T06:05:26', 'lrusso96', 'https://github.com/anuraghazra/github-readme-stats/pull/470'),
-(39, 278335273, 1, '2020-08-13T13:40:50', 'HerrEurobeat', 'https://github.com/anuraghazra/github-readme-stats/pull/382'),
-(6, 278335273, 1, '2020-07-15T15:07:43', 'JounQin', 'https://github.com/anuraghazra/github-readme-stats/pull/61'),
 (2788, 243394576, 1, '2020-12-11T04:56:06', 'etep', 'https://github.com/pixie-io/pixie/pull/178'),
 (24, 278335273, 2, '2021-04-26T18:52:14', 'mikebeaton', 'https://github.com/anuraghazra/github-readme-stats/pull/1015'),
 (21, 278335273, 2, '2020-10-04T14:47:14', 'jalenchuh', 'https://github.com/anuraghazra/github-readme-stats/pull/539'),
@@ -2556,6 +2554,8 @@ INSERT INTO contributions(id, repo_id, count, last_merged_at, contributor, url) 
 (2853, 27380215, 1, '2015-08-28T09:21:48', 'bamx23', 'https://github.com/vimagick/dockerfiles/pull/5'),
 (2854, 27380215, 1, '2019-10-26T23:35:44', 'gr1m', 'https://github.com/vimagick/dockerfiles/pull/93'),
 (2855, 27380215, 1, '2017-04-28T16:44:06', 'Badmuts', 'https://github.com/vimagick/dockerfiles/pull/41'),
+(39, 278335273, 1, '2020-08-13T13:40:50', 'HerrEurobeat', 'https://github.com/anuraghazra/github-readme-stats/pull/382'),
+(6, 278335273, 1, '2020-07-15T15:07:43', 'JounQin', 'https://github.com/anuraghazra/github-readme-stats/pull/61'),
 (2856, 27380215, 1, '2019-10-26T23:42:23', 'okainov', 'https://github.com/vimagick/dockerfiles/pull/90'),
 (2857, 27380215, 1, '2021-05-06T07:54:24', 'SuddenGunter', 'https://github.com/vimagick/dockerfiles/pull/156'),
 (2858, 27380215, 1, '2021-07-22T02:52:37', 'LaQuay', 'https://github.com/vimagick/dockerfiles/pull/132'),
--- src/seed/users_to_repos_votes.sql
+++ src/seed/users_to_repos_votes.sql
@@ -1,5 +1,5 @@
 --
--- Data for Name: users_to_repos_votes; Type: TABLE DATA; Schema: public; Timestamp: 2022-05-20T18:48:28.735Z
+-- Data for Name: users_to_repos_votes; Type: TABLE DATA; Schema: public; Timestamp: 2022-05-20T20:46:12.467Z
 -- COPY users_to_repos_votes(id, user_id, repo_id, created_at) FROM 'users_to_repos_votes.csv' WITH DELIMITER ',' CSV HEADER;
 --
 
