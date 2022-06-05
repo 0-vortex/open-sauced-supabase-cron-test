6763094114ac1ba953ca43f81f6e772d74c2ec4e
0-vortex <0-vortex@users.noreply.github.com>
2022-06-05 01:05:03 +0000

    chore: seed supabase data [skip ci]

--- src/seed/contributions.sql
+++ src/seed/contributions.sql
@@ -1,5 +1,5 @@
 --
--- Data for Name: contributions; Type: TABLE DATA; Schema: public; Timestamp: 2022-06-04T22:44:28.990Z
+-- Data for Name: contributions; Type: TABLE DATA; Schema: public; Timestamp: 2022-06-05T01:05:01.289Z
 -- COPY contributions(id, repo_id, count, last_merged_at, contributor, url) FROM 'contributions.csv' WITH DELIMITER ',' CSV HEADER;
 --
 
@@ -686,8 +686,8 @@ INSERT INTO contributions(id, repo_id, count, last_merged_at, contributor, url) 
 (22, 278335273, 2, '2020-12-31T06:37:57', 'mustafacagri', 'https://github.com/anuraghazra/github-readme-stats/pull/766'),
 (30, 278335273, 2, '2020-07-24T12:53:50', 'ooobsidian', 'https://github.com/anuraghazra/github-readme-stats/pull/172'),
 (4, 278335273, 4, '2022-05-04T07:42:29', 'qwerty541', 'https://github.com/anuraghazra/github-readme-stats/pull/956'),
-(32, 278335273, 3, '2020-07-24T07:17:42', 'terror', 'https://github.com/anuraghazra/github-readme-stats/pull/166'),
 (36, 278335273, 1, '2020-07-17T15:24:48', 'reed-jones', 'https://github.com/anuraghazra/github-readme-stats/pull/81'),
+(32, 278335273, 3, '2020-07-24T07:17:42', 'terror', 'https://github.com/anuraghazra/github-readme-stats/pull/166'),
 (26, 278335273, 2, '2021-01-07T14:22:18', 'Awilum', 'https://github.com/anuraghazra/github-readme-stats/pull/791'),
 (35, 278335273, 2, '2020-08-30T12:53:59', 'imrdong', 'https://github.com/anuraghazra/github-readme-stats/pull/418'),
 (14, 278335273, 2, '2020-07-25T12:56:16', 'sagar-gavhane', 'https://github.com/anuraghazra/github-readme-stats/pull/185'),
@@ -1459,11 +1459,11 @@ INSERT INTO contributions(id, repo_id, count, last_merged_at, contributor, url) 
 (4838, 507, 1, '2012-06-01T13:56:29', 'wjlroe', 'https://github.com/sferik/twitter/pull/261'),
 (3967, 404324019, 1, '2022-02-20T12:32:13', 'AminGholizad', 'https://github.com/arpit456jain/Getting-Started-with-open-source/pull/92'),
 (3974, 404324019, 1, '2021-10-01T08:50:48', 'manish831', 'https://github.com/arpit456jain/Getting-Started-with-open-source/pull/26'),
-(3973, 404324019, 1, '2021-10-03T03:48:39', 'vyomguptaa', 'https://github.com/arpit456jain/Getting-Started-with-open-source/pull/31'),
 (3966, 404324019, 2, '2022-03-25T12:35:48', 'ankit1111verma', 'https://github.com/arpit456jain/Getting-Started-with-open-source/pull/112'),
 (3978, 404324019, 1, '2021-10-26T12:17:58', 'Abhiyodaya2002', 'https://github.com/arpit456jain/Getting-Started-with-open-source/pull/51'),
 (3971, 404324019, 4, '2021-10-28T13:15:28', 'Mitali-13', 'https://github.com/arpit456jain/Getting-Started-with-open-source/pull/57'),
 (3975, 404324019, 2, '2021-10-09T09:33:52', 'ananyaredhu', 'https://github.com/arpit456jain/Getting-Started-with-open-source/pull/38'),
+(3973, 404324019, 1, '2021-10-03T03:48:39', 'vyomguptaa', 'https://github.com/arpit456jain/Getting-Started-with-open-source/pull/31'),
 (3968, 404324019, 2, '2022-02-08T15:05:04', 'Anubhavgupta14', 'https://github.com/arpit456jain/Getting-Started-with-open-source/pull/79'),
 (3976, 404324019, 1, '2021-10-01T17:43:29', 'neha-ajith', 'https://github.com/arpit456jain/Getting-Started-with-open-source/pull/28'),
 (3981, 404324019, 1, '2021-09-08T15:41:37', 'geeky01adarsh', 'https://github.com/arpit456jain/Getting-Started-with-open-source/pull/7'),
@@ -3866,8 +3866,8 @@ INSERT INTO contributions(id, repo_id, count, last_merged_at, contributor, url) 
 (4779, 279064706, 1, '2020-07-18T13:01:48', 'okankocyigit', 'https://github.com/abhisheknaiidu/awesome-github-profile-readme/pull/88'),
 (16124, 24344106, 2, '2021-09-24T14:18:28', 'thomasrockhu-codecov', 'https://github.com/codecov/example-python/pull/65'),
 (16125, 24344106, 1, '2020-08-05T19:20:52', 'thomasrockhu', 'https://github.com/codecov/example-python/pull/52'),
-(16126, 24344106, 1, '2021-10-04T22:58:07', 'cleder', 'https://github.com/codecov/example-python/pull/66'),
 (16127, 24344106, 1, '2020-10-16T16:12:56', 'MicaelJarniac', 'https://github.com/codecov/example-python/pull/58'),
+(16126, 24344106, 1, '2021-10-04T22:58:07', 'cleder', 'https://github.com/codecov/example-python/pull/66'),
 (16128, 24344106, 1, '2017-02-02T19:09:39', 'briandant', 'https://github.com/codecov/example-python/pull/20'),
 (16129, 24344106, 1, '2017-12-11T10:13:42', 'gundalow', 'https://github.com/codecov/example-python/pull/24'),
 (16130, 24344106, 1, '2015-06-29T21:32:43', 'willingc', 'https://github.com/codecov/example-python/pull/1'),
