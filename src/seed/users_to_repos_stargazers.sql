55e7c5cb8655e03c8955eac6ec08aadcceb62ac2
0-vortex <0-vortex@users.noreply.github.com>
2022-08-19 20:45:52 +0000

    chore: seed supabase data [skip ci]

---
+++ src/seed/users_to_repos_stargazers.sql
@@ -0,0 +1,7 @@
+--
+-- Data for Name: users_to_repos_stargazers; Type: TABLE DATA; Schema: public; Timestamp: 2022-08-19T20:45:49.423Z
+-- COPY users_to_repos_stargazers(id, user_id, repo_id, created_at, updated_at, deleted_at) FROM 'users_to_repos_stargazers.csv' WITH DELIMITER ',' CSV HEADER;
+--
+
+INSERT INTO users_to_repos_stargazers(id, user_id, repo_id, created_at, updated_at, deleted_at) VALUES
+(2, 237133, 39769, '2022-08-19T19:08:39.381208', '2022-08-19T19:08:39.381208', null);
---
+++ src/seed/users_to_repos_submissions.sql
@@ -0,0 +1,7 @@
+--
+-- Data for Name: users_to_repos_submissions; Type: TABLE DATA; Schema: public; Timestamp: 2022-08-19T20:45:49.136Z
+-- COPY users_to_repos_submissions(id, user_id, repo_id, created_at, updated_at, deleted_at) FROM 'users_to_repos_submissions.csv' WITH DELIMITER ',' CSV HEADER;
+--
+
+INSERT INTO users_to_repos_submissions(id, user_id, repo_id, created_at, updated_at, deleted_at) VALUES
+(1, 237133, 39769, '2022-08-19T19:05:34.036125+00:00', '2022-08-19T19:05:34.036125+00:00', null);
